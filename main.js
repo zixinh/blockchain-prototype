2d5caa50696a2d566130187bb017f7ee5eeb3690
Zixin Huang <huang.zixin@mail.utoronto.ca>
2019-06-27 22:50:58 -0400

    proof-of-work + transaction + mine reward implemented

--- main.js
+++ main.js
@@ -1,37 +1,95 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+
 class Block {
-  constructor(index, timestamp, data, previousHash = '') {
-    this.index = index;
+  constructor(timestamp, transactions, previousHash = '') {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.previousHash = previousHash;
     this.hash = '';
+    this.nonce = 0;
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(
+      this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.transactions) +
+        this.nonce
+    ).toString();
   }
 
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+  }
 }
 
 class Blockchain {
-  constructor() {
+  constructor(difficulty) {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = difficulty;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
   }
 
   createGenesisBlock() {
-    return new Block(0, "01/01/2019", "Genesis Block", "0");
+    return new Block('01/01/2019', 'Genesis Block', '0');
   }
 
   getLatestBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
-    this.chain.push(newBlock);
+  minePendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions);
+    block.mineBlock(this.difficulty);
+
+    console.log('BLOCK MINED:', block.hash);
+    console.log('Block successfully mined!');
+    this.chain.push(block);
+
+    if (
+      this.pendingTransactions.length === 1 &&
+      this.pendingTransactions[0].toAddress === miningRewardAddress
+    ) {
+      return;
+    }
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    ];
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress === address) {
+          balance -= trans.amount;
+        }
+        if (trans.toAddress === address) {
+          balance += trans.amount;
+        }
+      }
+    }
+    return balance;
   }
 
   isChainValid() {
@@ -51,25 +109,21 @@ class Blockchain {
   }
 }
 
+let huangCoin = new Blockchain(2);
+huangCoin.createTransaction(new Transaction('address1', 'address2', 100));
+huangCoin.createTransaction(new Transaction('address1', 'address2', 50));
 
-let newCoin = new Blockchain();
-newCoin.addBlock(new Block(1, "10/07/2019", { amount: 10 }));
-newCoin.addBlock(new Block(2, "12/07/2019", { amount: 20 }));
-newCoin.addBlock(new Block(3, "15/07/2019", { amount: 50 }));
+console.log('\n Starting the miner...');
+huangCoin.minePendingTransactions('mine-digger');
+console.log(
+  'Balance of mine-digger is',
+  huangCoin.getBalanceOfAddress('mine-digger')
+);
 
-// visualize our chain
-console.log(JSON.stringify(newCoin.chain, null, 4));
+console.log('\n Mining reward transaction....');
+huangCoin.minePendingTransactions('mine-digger');
 
-console.log('verify if newCoin is valid:');
-console.log('Is blockchain valid? ' + newCoin.isChainValid());
-
-console.log('change the data of a block, test validity:')
-newCoin.chain[1].data = { amount: 5 };
-console.log('Is blockchain valid? ' + newCoin.isChainValid());
-
-console.log('change the hash of a block, test validity:')
-newCoin.chain[1].hash = newCoin.chain[1].calculateHash();
-newCoin.chain[1].data = { amount: 10 };
-console.log('Is blockchain valid? ' + newCoin.isChainValid());
-
-
+console.log(
+  'Balance of mine-digger is',
+  huangCoin.getBalanceOfAddress('mine-digger')
+);
